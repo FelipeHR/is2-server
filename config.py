f4adf8f1f74f1fc2004ae01964df0a24bb6292b7
FelipeHR <42549396+FelipeHR@users.noreply.github.com>
2022-04-26 15:32:36 -0400

    Implementacion base de datos al codigo

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ from distutils.debug import DEBUG
 import os
 
 class Config(object):
+    MYSQL_HOST = 'remotemysql.com'
+    MYSQL_USER = 'givcGg9Yql'
+    MYSQL_PASSWORD = 'LHJOG3t1Ja'
+    MYSQL_DB = 'givcGg9Yql'
+
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USERNAME = 'onefelixe@gmail.com'
